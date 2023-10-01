4a45a8b0a60bd966a10d8a56d61613a208b8e848
Matheus Rodrigues <matheus.randrey@gmail.com>
2023-10-01 16:20:21 -0300

    Test and code button

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { logRoles } from "@testing-library/dom";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial color, and updates when click", () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: "Learn React" });
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeInTheDocument();
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to blue'
+  expect(colorButton).toHaveTextContent("Change to red");
 });
